9b2b79c8616ebb036c2a8916f4536f666d2c77bf
sun like <592134873@qq.com>
2020-03-24 14:48:05 +0800

    发布更新

--- source/_data/test.js
+++ source/_data/test.js
@@ -1,127 +1,22 @@
-
-// name = 'window'
-// function showName() {
-//     console.log(this.name)
-// }
-
-// var person1 = {
-//     name: 'person1',
-//     sayName: showName
-// }
-
-// var person2 = {
-//     name: 'person2',
-//     sayName: function() {
-//         const fun = person1.sayName
-//         fun()
-//     }
-// }
-
-// person1.sayName()
-// person2.sayName()
-
-// name = 'window'
-
-// function A() {
-//     this.name = 'FunctionA'
-//     this.getName = function() {
-//         console.log(this)
-//     }
-// }
+// 请编写一个JavaScript函数 parseQueryString，它的用途是把URL参数解析为一个对象，如：
+var url = "http://www.taobao.com/index.php?key0=0&key1=1&key2=2....."
+var obj = parseQueryString(url);
 
 
-// const a = new A()
-// const test = a.getName
-// test()
-
-// console.log(this)
-// name = 'window'
-
-// function getName() {
-//     console.log(this.name)
-// }
-
-// const a = {
-//     name: 'objA'
-// }
-
-// getName.apply(a)
-// getName.call(a)
-// getName.apply()
-// getName.call()
-
-
-
-// name = 'window'
-// b = () => {
-//     console.log(this)
-//     console.log(this.name)
-// }
-
-// console.log(this)
-
-
-
-// es5
-name = 'window'
-
-function A(name) {
-    this.name = name
-}
-A.prototype.getName = function() {
-    console.log(this.name)
+function parseQueryString(url) {
+    const result = url.split('?')
+    if(result.length < 2) {
+        return {}
+    }
+    const queryStr = result[1]
+    const queryArray = queryStr.split('&')
+    // console.log(queryArray)
+    let dict = {}
+    queryArray.forEach(element => {
+        const keyValues = element.split('=')
+        dict[keyValues[0]] = keyValues[1]
+    });
+    console.log(dict)
+    return dict
 }
 
-const a = new A('objA')
-const test = a.getName
-test()
-
-// es6
-name = 'window'
-class A {
-    constructor(name) {
-        this.name = name
-    }
-
-    getName() {
-        console.log(this.name)
-    }
-}
-
-const a = new A('objA')
-const test = a.getName
-test()
-
-
-class C {
-    constructor(name) {
-        this.name = name
-    }
-    getName = () => {
-        console.log(this.name)
-    }
-}
-
-const c = new C('ClassC')
-const test = c.getName
-test()
-
-
-// name = 'window'
-
-// const objA = {
-//     name: 'objA',
-//     getName: ()=>{
-//         console.log(this.name)
-//     }
-// }
-
-// const objB = {
-//     name: 'objB',
-//     getName: ()=>{
-//         console.log(this.name)
-//     }
-// }
-
-// const test = objA.getName
-// test()
